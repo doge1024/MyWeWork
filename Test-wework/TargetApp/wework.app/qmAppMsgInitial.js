ee145c5f93f97b3e26b0aab3cc6bade75c664afd
lizhihui <867129306@qq.com>
2017-12-19 23:29:16 +0800

    new

--- Test-wework/TargetApp/wework.app/qmAppMsgInitial.js
+++
@@ -1,7 +0,0 @@
-var currScale = 1,
-    isLandscape = false,
-    shouldScaleToFix = false,
-    screenWidth = screen.width;
-QMail.Editor.prototype._selectionToEndWhenWired = false;
-QMail.config.entranceType = "appmsg";
-QMail.NativeNotifier.validateMessages.keyUp = true;
--- Test-wework/TargetApp/wework.app/qmEMailInitial.js
+++
@@ -1,7 +0,0 @@
-var currScale = 1,
-    isLandscape = false,
-    shouldScaleToFix = false,
-    screenWidth = screen.width;
-QMail.Editor.prototype._selectionToEndWhenWired = false;
-QMail.config.entranceType = "mail";
-QMail.NativeNotifier.validateMessages.keyUp = true;
