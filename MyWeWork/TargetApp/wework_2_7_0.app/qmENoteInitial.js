da2299847506ad66bdc5b79e1d8211b6f44ac7e9
harddog <867129306@qq.com>
2019-01-18 20:18:36 +0800

    移除不必要文件

--- MyWeWork/TargetApp/wework_2_7_0.app/qmENoteInitial.js
+++
@@ -1,8 +0,0 @@
-var screenWidth = $screenWidth$,
-    currScale = $scale$,
-    isLandscape = $isLandscape$,
-    noteId = "$noteId$",
-    shouldScaleToFix = $shouldScaleToFix$;
-QMail.Editor.prototype._selectionToEndWhenWired = false;
-QMail.config.entranceType = "note";
-QMail.evaluator.becomBrowserFirstResponder = function () {};
